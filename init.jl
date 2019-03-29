00effbc2dac61c6a4a5d2133a9123e62ca9c55b5
Arnav Sood <soodarnav01@gmail.com>
2019-03-29 12:05:30 -0700

    move packagecompiler

--- init.jl
+++ init.jl
@@ -1,13 +1,10 @@
-using Pkg
-pkg"activate ."
-pkg"add PackageCompiler" 
-using PackageCompiler 
+using Pkg, PackageCompiler
 
-packages_to_precompile = [
+packages_to_precompile = (
     "Plots",
     "DataFrames",
     "GR",
     "Query"
-]
+)
 
 compile_package(packages_to_precompile..., force = true)
